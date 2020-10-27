451b54c816eec72af946a794396765665d3903eb
Carlos Ramirez <carlos@extrasistemas.com>
2020-10-27 14:36:00 -0600

    added decription by functions algoritmicas

--- complejidad_algoritmica2.py
+++ complejidad_algoritmica2.py
@@ -6,22 +6,22 @@ class ComplejidadAlgoritmica:
     
     def __init__(self, number):
         self.number = number
-
+    #O(1) Constante: no importa la cantidad de input que reciba, siempre demorara el mismo tiempo.
     def constante(self):
         return self.number
-
+    #O(n) Lineal: la complejidad crecerá de forma proporcional a medida que crezca el input.
     def logaritmica(self):
         return math.log10(self.number)
-
+    #O(log n) Logarítmica: nuestra función crecerá de forma logarítmica con respecto al input. Esto significa que en un inicio crecerá rápido, pero luego se estabilizara.
     def lineal(self):
         return self.number
-    
+    #O(n log n) Log lineal: crecerá de forma logarítmica pero junto con una constante.
     def log_lineal(self):
         return self.number * math.log10(self.number)
-
+    #O(n²) Polinomial: crecen de forma cuadrática. No son recomendables a menos que el input de datos en pequeño.
     def polinominal(self):
         return self.number**2
-    
+    #O(2^n) Exponencial: crecerá de forma exponencial, por lo que la carga es muy alta. Para nada recomendable en ningún caso, solo para análisis conceptual.
     def exponencial(self):
         return 2**self.number
 
